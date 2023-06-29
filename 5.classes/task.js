8eafc9c6c4d7575f4c2c7432ba98d18355bcda1d
Artem Kornishon <awaywee@gmail.com>
2023-06-29 22:11:30 +0300

    classes hw done

---
+++ 5.classes/task.js
@@ -0,0 +1,76 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount){
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix(){
+        return this.state*1.5;
+    }
+
+    set state(_state) {
+        if (_state < 0) {
+            this._state = 0;
+        } 
+        if (_state >= 100) {
+            this._state = 100;
+        }
+        if ((_state > 0) && (_state < 100)) {
+            this._state = _state;
+        }
+    } 
+
+    get state() {
+        return this._state;
+    }
+}
+
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount)
+        this.type = "Magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, author){
+        super(name, releaseDate, pagesCount)
+        this.type = "book";
+        this.author = author;
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'novel';
+    }
+}
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30){
+            this.books.push(book);
+        }
+    }
+    findBookBy(type, value) {
+        const findResult = this.books.find((item) => item[type] === value);
+        return findResult || null;
+    }
+
+    giveBookByName(bookName) {
+        const book = this.findBookBy("name", bookName);
+        if (!book) return null;
+        this.books = this.books.filter((item) => item.name !== bookName);
+        return book;
+    }
+}
